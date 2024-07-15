981500ac1becdb5e06179dd9475deb63974e8f34
engine <engine>
2024-07-15 17:13:49 +0800

    add kvserver

--- src/kvraft/common.go
+++ src/kvraft/common.go
@@ -4,6 +4,7 @@ const (
 	OK             = "OK"
 	ErrNoKey       = "ErrNoKey"
 	ErrWrongLeader = "ErrWrongLeader"
+	ErrTimeout     = "ErrTimeout"
 )
 
 type Err string
@@ -16,18 +17,25 @@ type PutAppendArgs struct {
 	// You'll have to add definitions here.
 	// Field names must start with capital letters,
 	// otherwise RPC will break.
+	ClientId int64
+	CmdNum   int // client为每个command分配的唯一序列号
 }
 
 type PutAppendReply struct {
-	Err Err
+	Err Err // error的字符串
+	//LeaderId int // 如果找错了leader则server将自己记录的leaderId附在reply里供client下一次直接定位
 }
 
 type GetArgs struct {
 	Key string
 	// You'll have to add definitions here.
+	ClientId int64
+	CmdNum   int // client为每个command分配的唯一序列号
 }
 
 type GetReply struct {
 	Err   Err
 	Value string
+	//LeaderId int // 如果找错了leader则server将自己记录的leaderId附在reply里供client下一次直接定位
 }
+
